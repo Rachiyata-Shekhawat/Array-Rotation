aad0034d2f37494bf885951d1c90432b7998de20
unknown <rjrachiyata@gmail.com>
2022-05-01 00:45:04 +0530

    js iile added

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+let array = [];
+
+function addElementHandler() {
+  let element = document.getElementById("element").value;
+
+  array.push(element);
+  // console.log(array);
+
+  document.getElementById("element").value = "";
+}
+
+// Displays original Array
+function showOriginalArrayHandler() {
+  if(array.length == 0) {
+    alert("Array is empty");
+  } else {
+    document.getElementById("originalArrayDisplay").innerText = "[" + array + "]" ;
+  }
+  
+}
+
+// Displays solution
+function showSolutionHandler() {
+  if(array.length == 0) {
+    alert("Array is empty");
+  } else if (array.length == 1) {
+    alert("There should be atleast 2 elements in your array");
+  } else {
+    const rotationCount = prompt("Enter the number of rotation you want !", 2);
+
+    for(j=0; j<rotationCount; j++) {
+      let lastElement = array[array.length-1];
+      // console.log(lastElement);
+
+      for(i=array.length-1 ;i>=0; i--) {
+        if (i==0) {
+          array[0] = lastElement;
+        } else {
+          array[i] = array[i-1];
+        }
+      }
+    }
+
+    document.getElementById("solutionDisplay").innerText = "[" + array + "]" ;
+  }
+}
